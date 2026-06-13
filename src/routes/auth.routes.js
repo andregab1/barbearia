dec29062a18d12121dbdbc69890c19992fe3502f
AndreGabriel <contatoandrealduan@gmail.com>
2026-06-13 17:42:06 -0300

    fix: auth routes refreshToken -> refresh

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,11 +2,11 @@
 // ROTAS: Autenticação
 // ==========================================
 const router = require('express').Router();
-const { cadastrar, login, refreshToken, logout } = require('../controllers/auth.controller');
+const { cadastrar, login, refresh, logout } = require('../controllers/auth.controller');
 
 router.post('/cadastrar', cadastrar);
 router.post('/login',     login);
-router.post('/refresh',   refreshToken);
+router.post('/refresh',   refresh);
 router.post('/logout',    logout);
 
 module.exports = router;
