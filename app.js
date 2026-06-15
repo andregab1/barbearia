8b8e2654dc7de25972f918de7eaeea07c839db6a
AndreGabriel <contatoandrealduan@gmail.com>
2026-06-14 23:35:58 -0300

    fix: aumentar limite payload para upload de logo

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const horariosRoutes      = require('./src/routes/horarios.routes');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'src/uploads')));
 
 // ==========================================
